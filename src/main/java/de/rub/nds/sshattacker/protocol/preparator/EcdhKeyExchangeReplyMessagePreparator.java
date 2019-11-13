760c35b507ad2788a7224ed814866fa21826193f
Daniel Hirschberger <daniel.hirschberger+git@rub.de>
2019-11-13 17:26:24 +0100

    Make EcdhKeyExchangeReply fuzzable

--- src/main/java/de/rub/nds/sshattacker/protocol/preparator/EcdhKeyExchangeReplyMessagePreparator.java
+++ src/main/java/de/rub/nds/sshattacker/protocol/preparator/EcdhKeyExchangeReplyMessagePreparator.java
@@ -3,6 +3,7 @@ package de.rub.nds.sshattacker.protocol.preparator;
 import de.rub.nds.sshattacker.constants.MessageIDConstant;
 import de.rub.nds.sshattacker.protocol.message.EcdhKeyExchangeReplyMessage;
 import de.rub.nds.sshattacker.state.SshContext;
+import java.math.BigInteger;
 
 public class EcdhKeyExchangeReplyMessagePreparator extends Preparator<EcdhKeyExchangeReplyMessage> {
 
@@ -13,18 +14,19 @@ public class EcdhKeyExchangeReplyMessagePreparator extends Preparator<EcdhKeyExc
     @Override
     public void prepare() {
         message.setMessageID(MessageIDConstant.SSH_MSG_KEX_ECDH_REPLY.id);
-//        message.setHostKeyLength(context.getDefaultRsaExponent().bitLength());
-//        message.setHostKeyType(context.getDefaultHostKeyType());
-//        message.setHostKeyTypeLength(context.getDefaultHostKeyType().length());
-//        message.setExponentLength(context.getDefaultRsaExponent().bitLength());
-//        message.setExponent(context.getDefaultRsaExponent());
-//        message.setModulus(context.getDefaultRsaModulus());
-//        message.setModulusLength(context.getDefaultRsaModulus().bitLength());
-//        message.setEphemeralPublicKey(context.getDefaultServerEcdhPublicKey());
-//        message.setEphemeralPublicKeyLength(context.getDefaultServerEcdhPublicKey().length);
-        // TODO implement signature calculation
-        //message.setSignature();
-        //message.setSignatureLength(0);
+
+        message.setHostKeyLength(Integer.MAX_VALUE);
+        message.setHostKeyType("");
+        message.setHostKeyTypeLength(Integer.MAX_VALUE);
+        message.setExponentLength(Integer.MAX_VALUE);
+        message.setExponent(BigInteger.ZERO);
+        message.setModulus(BigInteger.ZERO);
+        message.setModulusLength(Integer.MAX_VALUE);
+        message.setEphemeralPublicKey(new byte[0]);
+        message.setEphemeralPublicKeyLength(Integer.MAX_VALUE);
+//        TODO implement signature calculation
+        message.setSignature(new byte[0]);
+        message.setSignatureLength(Integer.MAX_VALUE);
     }
 
 }
