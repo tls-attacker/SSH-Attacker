1562586047881412559ebabefcab0ca2acb0406f
Fabian Bäumer <fabian.baeumer@rub.de>
2021-07-13 18:25:22 +0200

    tests: Add simple unit tests for ChannelOpenFailureMessage

---
+++ SSH-Core/src/test/java/de/rub/nds/sshattacker/core/protocol/cyclic/CyclicChannelOpenFailureTest.java
@@ -0,0 +1,47 @@
+/**
+ * SSH-Attacker - A Modular Penetration Testing Framework for SSH
+ *
+ * Copyright 2014-2021 Ruhr University Bochum, Paderborn University,
+ * and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.sshattacker.core.protocol.cyclic;
+
+import de.rub.nds.sshattacker.core.protocol.message.ChannelOpenFailureMessage;
+import de.rub.nds.sshattacker.core.protocol.parser.ChannelOpenFailureMessageParser;
+import de.rub.nds.sshattacker.core.protocol.parser.ChannelOpenFailureMessageParserTest;
+import de.rub.nds.sshattacker.core.protocol.serializer.ChannelOpenFailureMessageSerializer;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class CyclicChannelOpenFailureTest {
+    /**
+     * Provides a stream of test vectors for cyclic testing
+     *
+     * @return A stream of test vectors to feed the testCyclic unit test
+     */
+    public static Stream<Arguments> provideTestVectors() {
+        return ChannelOpenFailureMessageParserTest.provideTestVectors()
+                .map((vector) -> Arguments.of(vector.get()[0]));
+    }
+
+    /**
+     * Cyclic test for parsing and serializing of ChannelOpenFailureMessages
+     *
+     * @param providedBytes
+     *            Bytes to parse and serialize again
+     */
+    @ParameterizedTest
+    @MethodSource("provideTestVectors")
+    public void testCyclic(byte[] providedBytes) {
+        ChannelOpenFailureMessage msg = new ChannelOpenFailureMessageParser(0, providedBytes).parse();
+        assertArrayEquals(providedBytes, new ChannelOpenFailureMessageSerializer(msg).serialize());
+    }
+}
---
+++ SSH-Core/src/test/java/de/rub/nds/sshattacker/core/protocol/serializer/ChannelOpenFailureMessageSerializerTest.java
@@ -0,0 +1,61 @@
+/**
+ * SSH-Attacker - A Modular Penetration Testing Framework for SSH
+ *
+ * Copyright 2014-2021 Ruhr University Bochum, Paderborn University,
+ * and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.sshattacker.core.protocol.serializer;
+
+import de.rub.nds.sshattacker.core.constants.MessageIDConstant;
+import de.rub.nds.sshattacker.core.protocol.message.ChannelOpenFailureMessage;
+import de.rub.nds.sshattacker.core.protocol.parser.ChannelOpenFailureMessageParserTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ChannelOpenFailureMessageSerializerTest {
+    /**
+     * Provides a stream of test vectors for the ChannelOpenFailureMessageSerializer class
+     *
+     * @return A stream of test vectors to feed the testSerialize unit test
+     */
+    public static Stream<Arguments> provideTestVectors() {
+        return ChannelOpenFailureMessageParserTest.provideTestVectors();
+    }
+
+    /**
+     * Test of ChannelCloseMessageSerializer::serialize method
+     *
+     * @param expectedBytes
+     *            Expected output bytes of the serialize() call
+     * @param providedRecipientChannel
+     *            Recipient channel identifier
+     * @param providedReasonCode
+     *            Reason code
+     * @param providedReason
+     *            Reason string
+     * @param providedLanguageTag
+     *            Language tag string
+     */
+    @ParameterizedTest
+    @MethodSource("provideTestVectors")
+    public void testSerialize(byte[] expectedBytes, int providedRecipientChannel, int providedReasonCode,
+            String providedReason, String providedLanguageTag) {
+        ChannelOpenFailureMessage msg = new ChannelOpenFailureMessage();
+        msg.setMessageID(MessageIDConstant.SSH_MSG_CHANNEL_OPEN_FAILURE.id);
+        msg.setRecipientChannel(providedRecipientChannel);
+        msg.setReasonCode(providedReasonCode);
+        msg.setReason(providedReason);
+        msg.setLanguageTag(providedLanguageTag);
+        ChannelOpenFailureMessageSerializer serializer = new ChannelOpenFailureMessageSerializer(msg);
+
+        assertArrayEquals(expectedBytes, serializer.serialize());
+    }
+}
