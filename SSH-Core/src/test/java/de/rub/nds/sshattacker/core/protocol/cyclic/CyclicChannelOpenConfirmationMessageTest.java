5169cfe9c4f2eed35c3161ba6dd4175d0189115d
Fabian Bäumer <fabian.baeumer@rub.de>
2021-07-21 18:34:44 +0200

    tests: Add simple unit tests for ChannelOpenConfirmationMessage

--- SSH-Core/src/test/java/de/rub/nds/sshattacker/core/protocol/cyclic/CyclicChannelOpenConfirmationMessageTest.java
+++ SSH-Core/src/test/java/de/rub/nds/sshattacker/core/protocol/cyclic/CyclicChannelOpenConfirmationMessageTest.java
@@ -9,10 +9,10 @@
  */
 package de.rub.nds.sshattacker.core.protocol.cyclic;
 
-import de.rub.nds.sshattacker.core.protocol.message.ChannelOpenFailureMessage;
-import de.rub.nds.sshattacker.core.protocol.parser.ChannelOpenFailureMessageParser;
-import de.rub.nds.sshattacker.core.protocol.parser.ChannelOpenFailureMessageParserTest;
-import de.rub.nds.sshattacker.core.protocol.serializer.ChannelOpenFailureMessageSerializer;
+import de.rub.nds.sshattacker.core.protocol.message.ChannelOpenConfirmationMessage;
+import de.rub.nds.sshattacker.core.protocol.parser.ChannelOpenConfirmationMessageParser;
+import de.rub.nds.sshattacker.core.protocol.parser.ChannelOpenConfirmationMessageParserTest;
+import de.rub.nds.sshattacker.core.protocol.serializer.ChannelOpenConfirmationMessageSerializer;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -21,19 +21,19 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class CyclicChannelOpenFailureTest {
+public class CyclicChannelOpenConfirmationMessageTest {
     /**
      * Provides a stream of test vectors for cyclic testing
      *
      * @return A stream of test vectors to feed the testCyclic unit test
      */
     public static Stream<Arguments> provideTestVectors() {
-        return ChannelOpenFailureMessageParserTest.provideTestVectors()
+        return ChannelOpenConfirmationMessageParserTest.provideTestVectors()
                 .map((vector) -> Arguments.of(vector.get()[0]));
     }
 
     /**
-     * Cyclic test for parsing and serializing of ChannelOpenFailureMessages
+     * Cyclic test for parsing and serializing of ChannelOpenConfirmationMessages
      *
      * @param providedBytes
      *            Bytes to parse and serialize again
@@ -41,7 +41,7 @@ public class CyclicChannelOpenFailureTest {
     @ParameterizedTest
     @MethodSource("provideTestVectors")
     public void testCyclic(byte[] providedBytes) {
-        ChannelOpenFailureMessage msg = new ChannelOpenFailureMessageParser(0, providedBytes).parse();
-        assertArrayEquals(providedBytes, new ChannelOpenFailureMessageSerializer(msg).serialize());
+        ChannelOpenConfirmationMessage msg = new ChannelOpenConfirmationMessageParser(0, providedBytes).parse();
+        assertArrayEquals(providedBytes, new ChannelOpenConfirmationMessageSerializer(msg).serialize());
     }
 }
---
+++ SSH-Core/src/test/java/de/rub/nds/sshattacker/core/protocol/serializer/ChannelOpenConfirmationMessageSerializerTest.java
@@ -0,0 +1,61 @@
+/**
+ * SSH-Attacker - A Modular Penetration Testing Framework for SSH
+ *
+ * Copyright 2014-2021 Ruhr University Bochum, Paderborn University,
+ * and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.sshattacker.core.protocol.serializer;
+
+import de.rub.nds.sshattacker.core.constants.MessageIDConstant;
+import de.rub.nds.sshattacker.core.protocol.message.ChannelOpenConfirmationMessage;
+import de.rub.nds.sshattacker.core.protocol.parser.ChannelOpenConfirmationMessageParserTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ChannelOpenConfirmationMessageSerializerTest {
+    /**
+     * Provides a stream of test vectors for the ChannelOpenConfirmationMessage class
+     *
+     * @return A stream of test vectors to feed the testSerialize unit test
+     */
+    public static Stream<Arguments> provideTestVectors() {
+        return ChannelOpenConfirmationMessageParserTest.provideTestVectors();
+    }
+
+    /**
+     * Test of ChannelOpenConfirmationMessage::serialize method
+     *
+     * @param expectedBytes
+     *            Expected output bytes of the serialize() call
+     * @param providedRecipientChannel
+     *            Recipient channel number
+     * @param providedSenderChannel
+     *            Sender channel number
+     * @param providedInitialWindowSize
+     *            Initial window size
+     * @param providedMaximumPacketSize
+     *            Maximum packet size
+     */
+    @ParameterizedTest
+    @MethodSource("provideTestVectors")
+    public void testSerialize(byte[] expectedBytes, int providedRecipientChannel, int providedSenderChannel,
+            int providedInitialWindowSize, int providedMaximumPacketSize) {
+        ChannelOpenConfirmationMessage msg = new ChannelOpenConfirmationMessage();
+        msg.setMessageID(MessageIDConstant.SSH_MSG_CHANNEL_OPEN_CONFIRMATION.id);
+        msg.setRecipientChannel(providedRecipientChannel);
+        msg.setSenderChannel(providedSenderChannel);
+        msg.setWindowSize(providedInitialWindowSize);
+        msg.setPacketSize(providedMaximumPacketSize);
+        ChannelOpenConfirmationMessageSerializer serializer = new ChannelOpenConfirmationMessageSerializer(msg);
+
+        assertArrayEquals(expectedBytes, serializer.serialize());
+    }
+}
