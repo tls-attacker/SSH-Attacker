a26540465a705e5e18db0b9af070616803c47714
Fabian Bäumer <fabian.baeumer@rub.de>
2021-08-09 23:49:59 +0200

    fix[DhGexKeyExchangeGroupMessage]: Exception avoidance

--- SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/handler/DhGexKeyExchangeGroupMessageHandler.java
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/handler/DhGexKeyExchangeGroupMessageHandler.java
@@ -15,8 +15,12 @@ import de.rub.nds.sshattacker.core.exceptions.AdjustmentException;
 import de.rub.nds.sshattacker.core.protocol.common.Handler;
 import de.rub.nds.sshattacker.core.protocol.transport.message.DhGexKeyExchangeGroupMessage;
 import de.rub.nds.sshattacker.core.state.SshContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class DhGexKeyExchangeGroupMessageHandler extends Handler<DhGexKeyExchangeGroupMessage> {
+
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public DhGexKeyExchangeGroupMessageHandler(SshContext context) {
         super(context);
@@ -24,21 +28,35 @@ public class DhGexKeyExchangeGroupMessageHandler extends Handler<DhGexKeyExchang
 
     @Override
     public void handle(DhGexKeyExchangeGroupMessage msg) {
-        DhKeyExchange dhKeyExchange =
-                (DhKeyExchange)
-                        context.getKeyExchangeInstance().orElseThrow(AdjustmentException::new);
-        dhKeyExchange.setModulus(msg.getGroupModulus().getValue());
-        dhKeyExchange.setGenerator(msg.getGroupGenerator().getValue());
+        if (context.getKeyExchangeInstance().isPresent()) {
+            DhKeyExchange dhKeyExchange = (DhKeyExchange) context.getKeyExchangeInstance().get();
+            dhKeyExchange.setModulus(msg.getGroupModulus().getValue());
+            dhKeyExchange.setGenerator(msg.getGroupGenerator().getValue());
+        } else {
+            String errorMsg = "Key exchange instance is not present, unable to set group modulus and generator for the key exchange instance";
+            if(context.getConfig().getAvoidAdjustmentExceptions()) {
+                LOGGER.warn(errorMsg);
+            } else {
+                throw new AdjustmentException(errorMsg);
+            }
+        }
 
         ExchangeHash exchangeHash = context.getExchangeHashInstance();
         if (exchangeHash instanceof DhGexExchangeHash) {
             DhGexExchangeHash dhGexExchangeHash = (DhGexExchangeHash) exchangeHash;
             dhGexExchangeHash.setGroupModulus(msg.getGroupModulus().getValue());
             dhGexExchangeHash.setGroupGenerator(msg.getGroupGenerator().getValue());
-        } else {
+        } else if (exchangeHash instanceof DhGexOldExchangeHash) {
             DhGexOldExchangeHash dhGexOldExchangeHash = (DhGexOldExchangeHash) exchangeHash;
             dhGexOldExchangeHash.setGroupModulus(msg.getGroupModulus().getValue());
             dhGexOldExchangeHash.setGroupGenerator(msg.getGroupGenerator().getValue());
+        } else {
+            String errorMsg = "Exchange hash instance is neither DhGexExchangeHash nor DhGexOldExchangeHash or key exchange instance is not present, unable to update exchange hash";
+            if(context.getConfig().getAvoidAdjustmentExceptions()) {
+                LOGGER.warn(errorMsg);
+            } else {
+                throw new AdjustmentException(errorMsg);
+            }
         }
     }
 }
