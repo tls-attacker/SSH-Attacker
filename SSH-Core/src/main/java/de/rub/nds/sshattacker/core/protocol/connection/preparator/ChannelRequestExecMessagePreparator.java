834814aaf60896aacf13c23ecde2fab323a265f8
Fabian Bäumer <fabian.baeumer@rub.de>
2021-09-13 16:12:54 +0200

    fix[ChannelRequestExecMessagePreparator]: Exception avoidance

--- SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/connection/preparator/ChannelRequestExecMessagePreparator.java
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/connection/preparator/ChannelRequestExecMessagePreparator.java
@@ -9,10 +9,10 @@ package de.rub.nds.sshattacker.core.protocol.connection.preparator;
 
 import de.rub.nds.sshattacker.core.constants.ChannelRequestType;
 import de.rub.nds.sshattacker.core.constants.MessageIDConstant;
-import de.rub.nds.sshattacker.core.exceptions.PreparationException;
 import de.rub.nds.sshattacker.core.protocol.common.Preparator;
 import de.rub.nds.sshattacker.core.protocol.connection.message.ChannelRequestExecMessage;
 import de.rub.nds.sshattacker.core.state.SshContext;
+import java.util.Optional;
 
 public class ChannelRequestExecMessagePreparator extends Preparator<ChannelRequestExecMessage> {
 
@@ -24,8 +24,14 @@ public class ChannelRequestExecMessagePreparator extends Preparator<ChannelReque
     @Override
     public void prepare() {
         message.setMessageID(MessageIDConstant.SSH_MSG_CHANNEL_REQUEST);
-        message.setRecipientChannel(
-                context.getRemoteChannel().orElseThrow(PreparationException::new));
+        Optional<Integer> remoteChannel = context.getRemoteChannel();
+        if (remoteChannel.isPresent()) {
+            message.setRecipientChannel(remoteChannel.get());
+        } else {
+            raisePreparationException(
+                    "Unable to prepare ChannelRequestExecMessage - No remote channel id set");
+            message.setRecipientChannel(0);
+        }
         message.setWantReply(context.getConfig().getReplyWanted());
         message.setRequestType(ChannelRequestType.EXEC, true);
         message.setCommand(context.getConfig().getChannelCommand(), true);
