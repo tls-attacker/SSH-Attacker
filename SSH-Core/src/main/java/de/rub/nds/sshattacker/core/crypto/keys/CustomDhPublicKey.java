78082039636ebc010d9aed79b11229feddc64e47
Fabian Bäumer <fabian.baeumer@rub.de>
2021-10-21 17:01:55 +0200

    doc[license]: Fix license header

--- SSH-Core/src/main/java/de/rub/nds/sshattacker/core/crypto/keys/CustomDhPublicKey.java
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/crypto/keys/CustomDhPublicKey.java
@@ -1,9 +1,9 @@
-/**
+/*
  * SSH-Attacker - A Modular Penetration Testing Framework for SSH
  *
- * <p>Copyright 2014-2021 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ * Copyright 2014-2021 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
  *
- * <p>Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
  */
 package de.rub.nds.sshattacker.core.crypto.keys;
 
