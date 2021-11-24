29e4f770fa889166fdf4ef79a107d0c829fc9b0b
Fabian Bäumer <fabian.baeumer@rub.de>
2021-11-24 18:28:40 +0100

    feat: Remove UnlimitedStrengthEnabler.java

--- SSH-Core/src/main/java/de/rub/nds/sshattacker/core/config/delegate/GeneralDelegate.java
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/config/delegate/GeneralDelegate.java
@@ -9,7 +9,6 @@ package de.rub.nds.sshattacker.core.config.delegate;
 
 import com.beust.jcommander.Parameter;
 import de.rub.nds.sshattacker.core.config.Config;
-import de.rub.nds.sshattacker.core.util.UnlimitedStrengthEnabler;
 import java.security.Provider;
 import java.security.Security;
 import org.apache.logging.log4j.Level;
@@ -72,9 +71,5 @@ public class GeneralDelegate extends Delegate {
         for (Provider p : Security.getProviders()) {
             LOGGER.debug("Provider {}, version, {}", p.getName(), p.getVersionStr());
         }
-
-        // remove stupid Oracle JDK security restriction (otherwise, it is not
-        // possible to use strong crypto with Oracle JDK)
-        UnlimitedStrengthEnabler.enable();
     }
 }
