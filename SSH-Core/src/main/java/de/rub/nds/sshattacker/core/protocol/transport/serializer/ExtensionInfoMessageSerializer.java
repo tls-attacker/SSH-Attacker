6a8fc33e790a82f9246fb4cecb25f59d1dc4c98a
nayirmp3 <Mahsum.Nayir@rub.de>
2023-04-26 16:17:11 +0200

    implemented serializer for ExtensionInfoMessage

--- SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/serializer/ExtensionInfoMessageSerializer.java
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/serializer/ExtensionInfoMessageSerializer.java
@@ -8,17 +8,72 @@
 package de.rub.nds.sshattacker.core.protocol.transport.serializer;
 
 import de.rub.nds.sshattacker.core.constants.DataFormatConstants;
+import de.rub.nds.sshattacker.core.constants.Extension;
 import de.rub.nds.sshattacker.core.protocol.common.SshMessageSerializer;
 import de.rub.nds.sshattacker.core.protocol.transport.message.ExtensionInfoMessage;
+import de.rub.nds.sshattacker.core.protocol.transport.message.extension.AbstractExtension;
+import de.rub.nds.sshattacker.core.protocol.transport.message.extension.DelayCompressionExtension;
+import de.rub.nds.sshattacker.core.protocol.transport.message.extension.ServerSigAlgsExtension;
+import de.rub.nds.sshattacker.core.protocol.transport.message.extension.UnknownExtension;
+import de.rub.nds.sshattacker.core.protocol.transport.serializer.extension.AbstractExtensionSerializer;
+import de.rub.nds.sshattacker.core.protocol.transport.serializer.extension.DelayCompressionExtensionSerializer;
+import de.rub.nds.sshattacker.core.protocol.transport.serializer.extension.ServerSigAlgsExtensionSerializer;
+import de.rub.nds.sshattacker.core.protocol.transport.serializer.extension.UnknownExtensionSerializer;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ExtensionInfoMessageSerializer extends SshMessageSerializer<ExtensionInfoMessage> {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public ExtensionInfoMessageSerializer(ExtensionInfoMessage message) {
         super(message);
     }
 
+    private void serializeExtensionCount() {
+        appendInt(message.getExtensionCount().getValue(), DataFormatConstants.UINT32_SIZE);
+        LOGGER.debug("Extension count: {}", message.getExtensionCount().getValue());
+    }
+
+    private void serializeExtensions() {
+        List<AbstractExtension<?>> extensions = message.getExtensions();
+        for (int extensionIndex = 0;
+                extensionIndex < message.getExtensionCount().getValue();
+                extensionIndex++) {
+            // determine extension for serializer
+            Extension extension =
+                    Extension.fromName(extensions.get(extensionIndex).getName().getValue());
+            // serialization for each individual extension
+            AbstractExtensionSerializer<?> extensionSerializer;
+            switch (extension) {
+                case SERVER_SIG_ALGS:
+                    extensionSerializer =
+                            new ServerSigAlgsExtensionSerializer(
+                                    (ServerSigAlgsExtension) extensions.get(extensionIndex));
+                    break;
+                case DELAY_COMPRESSION:
+                    extensionSerializer =
+                            new DelayCompressionExtensionSerializer(
+                                    (DelayCompressionExtension) extensions.get(extensionIndex));
+                    break;
+                default:
+                    LOGGER.debug(
+                            "Extension [{}] (index {}) is unknown or not implemented, serializing as UnknownExtension",
+                            extension,
+                            extensionIndex);
+                    extensionSerializer =
+                            new UnknownExtensionSerializer(
+                                    (UnknownExtension) extensions.get(extensionIndex));
+                    break;
+            }
+            appendBytes(extensionSerializer.serialize());
+        }
+    }
+
     @Override
     public void serializeMessageSpecificContents() {
-        appendInt(message.getExtensionCount().getValue(), DataFormatConstants.UINT32_SIZE);
-        // TODO: Add serialization for each individual extension
+        serializeExtensionCount();
+        serializeExtensions();
     }
 }
