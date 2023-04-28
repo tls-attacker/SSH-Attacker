1a0a93ff72550ee7610a9f910c35285110d8679f
nayirmp3 <Mahsum.Nayir@rub.de>
2023-04-28 12:33:25 +0200

    implemented preparator for delay-compression extension

---
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/preparator/extension/DelayCompressionExtensionPreparator.java
@@ -0,0 +1,43 @@
+/*
+ * SSH-Attacker - A Modular Penetration Testing Framework for SSH
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.sshattacker.core.protocol.transport.preparator.extension;
+
+import de.rub.nds.sshattacker.core.constants.CompressionMethod;
+import de.rub.nds.sshattacker.core.protocol.transport.message.extension.DelayCompressionExtension;
+import de.rub.nds.sshattacker.core.util.Converter;
+import de.rub.nds.sshattacker.core.workflow.chooser.Chooser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DelayCompressionExtensionPreparator
+        extends AbstractExtensionPreparator<DelayCompressionExtension> {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public DelayCompressionExtensionPreparator(
+            Chooser chooser, DelayCompressionExtension extension) {
+        super(chooser, extension);
+    }
+
+    @Override
+    protected void prepareExtensionSpecificContents() {
+        if (chooser.getContext().isClient()) {
+            chooser.getContext()
+                    .setClientSupportedDelayCompressionMethods(
+                            Converter.nameListToEnumValues(
+                                    getObject().getCompressionMethodsClientToServer().getValue(),
+                                    CompressionMethod.class));
+        } else {
+            chooser.getContext()
+                    .setServerSupportedDelayCompressionMethods(
+                            Converter.nameListToEnumValues(
+                                    getObject().getCompressionMethodsServerToClient().getValue(),
+                                    CompressionMethod.class));
+        }
+    }
+}
