286be00fd2087bb58318d4b1e57afab094df5725
nayirmp3 <Mahsum.Nayir@rub.de>
2023-04-28 11:18:29 +0200

    implemented abstract class for preparing extensions

---
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/preparator/extension/AbstractExtensionPreparator.java
@@ -0,0 +1,31 @@
+/*
+ * SSH-Attacker - A Modular Penetration Testing Framework for SSH
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.sshattacker.core.protocol.transport.preparator.extension;
+
+import de.rub.nds.sshattacker.core.protocol.common.Preparator;
+import de.rub.nds.sshattacker.core.protocol.transport.message.extension.AbstractExtension;
+import de.rub.nds.sshattacker.core.workflow.chooser.Chooser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public abstract class AbstractExtensionPreparator<E extends AbstractExtension>
+        extends Preparator<E> {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public AbstractExtensionPreparator(Chooser chooser, E extension) {
+        super(chooser, extension);
+    }
+
+    @Override
+    public void prepare() {
+        prepareExtensionSpecificContents();
+    }
+
+    protected abstract void prepareExtensionSpecificContents();
+}
