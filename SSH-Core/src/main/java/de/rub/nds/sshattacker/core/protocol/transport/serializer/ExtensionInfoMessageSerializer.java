375d591526e2acdb8e147a2d3a67b2120e679700
nayirmp3 <Mahsum.Nayir@rub.de>
2023-05-05 12:44:22 +0200

    changed serialize function for ExtensionInfoMessage
    removed for-loop and replaced with code using handler to determine serializer to use

--- SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/serializer/ExtensionInfoMessageSerializer.java
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/serializer/ExtensionInfoMessageSerializer.java
@@ -8,15 +8,8 @@
 package de.rub.nds.sshattacker.core.protocol.transport.serializer;
 
 import de.rub.nds.sshattacker.core.constants.DataFormatConstants;
-import de.rub.nds.sshattacker.core.constants.Extension;
 import de.rub.nds.sshattacker.core.protocol.common.SshMessageSerializer;
 import de.rub.nds.sshattacker.core.protocol.transport.message.ExtensionInfoMessage;
-import de.rub.nds.sshattacker.core.protocol.transport.message.extension.AbstractExtension;
-import de.rub.nds.sshattacker.core.protocol.transport.serializer.extension.AbstractExtensionSerializer;
-import de.rub.nds.sshattacker.core.protocol.transport.serializer.extension.DelayCompressionExtensionSerializer;
-import de.rub.nds.sshattacker.core.protocol.transport.serializer.extension.ServerSigAlgsExtensionSerializer;
-import de.rub.nds.sshattacker.core.protocol.transport.serializer.extension.UnknownExtensionSerializer;
-import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -34,38 +27,11 @@ public class ExtensionInfoMessageSerializer extends SshMessageSerializer<Extensi
     }
 
     private void serializeExtensions() {
-        List<AbstractExtension<?>> extensions = message.getExtensions();
-        for (int extensionIndex = 0;
-                extensionIndex < message.getExtensionCount().getValue();
-                extensionIndex++) {
-            // determine extension for serializer
-            Extension extension =
-                    Extension.fromName(extensions.get(extensionIndex).getName().getValue());
-            // serialization for each individual extension
-            AbstractExtensionSerializer<?> extensionSerializer;
-            switch (extension) {
-                case SERVER_SIG_ALGS:
-                    extensionSerializer =
-                            (ServerSigAlgsExtensionSerializer)
-                                    extensions.get(extensionIndex).getHandler(null).getSerializer();
-                    break;
-                case DELAY_COMPRESSION:
-                    extensionSerializer =
-                            (DelayCompressionExtensionSerializer)
-                                    extensions.get(extensionIndex).getHandler(null).getSerializer();
-                    break;
-                default:
-                    LOGGER.debug(
-                            "Extension [{}] (index {}) is unknown or not implemented, serializing as UnknownExtension",
-                            extension,
-                            extensionIndex);
-                    extensionSerializer =
-                            (UnknownExtensionSerializer)
-                                    extensions.get(extensionIndex).getHandler(null).getSerializer();
-                    break;
-            }
-            appendBytes(extensionSerializer.serialize());
-        }
+        message.getExtensions().stream()
+                .forEachOrdered(
+                        extension -> {
+                            appendBytes(extension.getHandler(null).getSerializer().serialize());
+                        });
     }
 
     @Override
