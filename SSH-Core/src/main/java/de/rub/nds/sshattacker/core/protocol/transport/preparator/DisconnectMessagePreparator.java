9c275cc445e9834d1fbb7ff4d0bf045adba7c506
nayirmp3 <Mahsum.Nayir@rub.de>
2023-05-11 12:45:14 +0200

    added if-statement
    if-statement for checking whether negotiation of compression method in delay-compression extension failed and then setting the description and reason code only when negotiation failed

--- SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/preparator/DisconnectMessagePreparator.java
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/preparator/DisconnectMessagePreparator.java
@@ -7,6 +7,7 @@
  */
 package de.rub.nds.sshattacker.core.protocol.transport.preparator;
 
+import de.rub.nds.sshattacker.core.constants.DisconnectReason;
 import de.rub.nds.sshattacker.core.constants.MessageIdConstant;
 import de.rub.nds.sshattacker.core.protocol.common.SshMessagePreparator;
 import de.rub.nds.sshattacker.core.protocol.transport.message.DisconnectMessage;
@@ -21,14 +22,14 @@ public class DisconnectMessagePreparator extends SshMessagePreparator<Disconnect
     @Override
     public void prepareMessageSpecificContents() {
         // TODO save values in config
-        /*
         getObject().setReasonCode(DisconnectReason.SSH_DISCONNECT_PROTOCOL_ERROR);
         getObject().setDescription("Test", true);
         getObject().setLanguageTag("", true);
-         */
 
-        getObject().setReasonCode(getObject().getReasonCode());
-        getObject().setDescription(getObject().getDescription(), true);
-        getObject().setLanguageTag(getObject().getLanguageTag(), true);
+        if (chooser.getContext().getDelayCompressionExtensionNegotiationFailed()) {
+            getObject().setReasonCode(getObject().getReasonCode());
+            getObject().setDescription(getObject().getDescription(), true);
+            getObject().setLanguageTag(getObject().getLanguageTag(), true);
+        }
     }
 }
