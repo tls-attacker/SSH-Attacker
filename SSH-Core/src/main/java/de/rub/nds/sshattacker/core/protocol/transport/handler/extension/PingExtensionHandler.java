98f78a8b41eb21c7f8aa8e381535f0d724169bd4
Fabian Bäumer <fabian.baeumer@rub.de>
2023-10-04 16:50:37 +0200

    feat: Implement ping@openssh.com extension

---
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/handler/extension/PingExtensionHandler.java
@@ -0,0 +1,55 @@
+/*
+ * SSH-Attacker - A Modular Penetration Testing Framework for SSH
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.sshattacker.core.protocol.transport.handler.extension;
+
+import de.rub.nds.sshattacker.core.protocol.transport.message.extension.PingExtension;
+import de.rub.nds.sshattacker.core.protocol.transport.parser.extension.PingExtensionParser;
+import de.rub.nds.sshattacker.core.protocol.transport.preparator.extension.PingExtensionPreparator;
+import de.rub.nds.sshattacker.core.protocol.transport.serializer.extension.PingExtensionSerializer;
+import de.rub.nds.sshattacker.core.state.SshContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PingExtensionHandler extends AbstractExtensionHandler<PingExtension> {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public PingExtensionHandler(SshContext context) {
+        super(context);
+    }
+
+    public PingExtensionHandler(SshContext context, PingExtension extension) {
+        super(context, extension);
+    }
+
+    @Override
+    public void adjustContext() {
+        LOGGER.info(
+                "Remote peer signaled support for ping@openssh.com extension via SSH_MSG_EXT_INFO");
+    }
+
+    @Override
+    public PingExtensionParser getParser(byte[] array) {
+        return new PingExtensionParser(array);
+    }
+
+    @Override
+    public PingExtensionParser getParser(byte[] array, int startPosition) {
+        return new PingExtensionParser(array, startPosition);
+    }
+
+    @Override
+    public PingExtensionPreparator getPreparator() {
+        return new PingExtensionPreparator(context.getChooser(), extension);
+    }
+
+    @Override
+    public PingExtensionSerializer getSerializer() {
+        return new PingExtensionSerializer(extension);
+    }
+}
---
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/transport/message/extension/PingExtension.java
@@ -0,0 +1,65 @@
+/*
+ * SSH-Attacker - A Modular Penetration Testing Framework for SSH
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.sshattacker.core.protocol.transport.message.extension;
+
+import de.rub.nds.modifiablevariable.ModifiableVariableFactory;
+import de.rub.nds.modifiablevariable.integer.ModifiableInteger;
+import de.rub.nds.modifiablevariable.string.ModifiableString;
+import de.rub.nds.sshattacker.core.protocol.transport.handler.extension.PingExtensionHandler;
+import de.rub.nds.sshattacker.core.state.SshContext;
+import java.nio.charset.StandardCharsets;
+
+public class PingExtension extends AbstractExtension<PingExtension> {
+
+    private ModifiableInteger versionLength;
+    private ModifiableString version;
+
+    public ModifiableInteger getVersionLength() {
+        return versionLength;
+    }
+
+    public void setVersionLength(ModifiableInteger versionLength) {
+        this.versionLength = versionLength;
+    }
+
+    public void setVersionLength(int versionLength) {
+        this.versionLength =
+                ModifiableVariableFactory.safelySetValue(this.versionLength, versionLength);
+    }
+
+    public ModifiableString getVersion() {
+        return version;
+    }
+
+    public void setVersion(ModifiableString version) {
+        setVersion(version, false);
+    }
+
+    public void setVersion(ModifiableString version, boolean adjustLengthField) {
+        if (adjustLengthField) {
+            setVersionLength(version.getValue().getBytes(StandardCharsets.US_ASCII).length);
+        }
+        this.version = version;
+    }
+
+    public void setVersion(String version) {
+        setVersion(version, false);
+    }
+
+    public void setVersion(String version, boolean adjustLengthField) {
+        if (adjustLengthField) {
+            setVersionLength(version.getBytes(StandardCharsets.US_ASCII).length);
+        }
+        this.version = ModifiableVariableFactory.safelySetValue(this.version, version);
+    }
+
+    @Override
+    public PingExtensionHandler getHandler(SshContext context) {
+        return new PingExtensionHandler(context, this);
+    }
+}
