e1244f1fdf449ef92a19531566f521fd15894bf6
c0d3d3v <c0d3d3v@mag-keinen-spam.de>
2025-03-16 12:14:32 +0100

    Add ChannelRequestMessageHandler class, that I forgot to add to the commit.

---
+++ SSH-Core/src/main/java/de/rub/nds/sshattacker/core/protocol/connection/handler/ChannelRequestMessageHandler.java
@@ -0,0 +1,32 @@
+/*
+ * SSH-Attacker - A Modular Penetration Testing Framework for SSH
+ *
+ * Copyright 2014-2022 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.sshattacker.core.protocol.connection.handler;
+
+import de.rub.nds.sshattacker.core.protocol.common.MessageSentHandler;
+import de.rub.nds.sshattacker.core.protocol.common.SshMessageHandler;
+import de.rub.nds.sshattacker.core.protocol.connection.message.ChannelRequestMessage;
+import de.rub.nds.sshattacker.core.state.SshContext;
+import de.rub.nds.sshattacker.core.util.Converter;
+
+public abstract class ChannelRequestMessageHandler<T extends ChannelRequestMessage<T>>
+        extends SshMessageHandler<T> implements MessageSentHandler<T> {
+
+    @Override
+    public void adjustContext(SshContext context, T object) {
+        if (Converter.byteToBoolean(object.getWantReply().getValue())) {
+            context.getChannelManager().addReceivedRequestThatWantsReply(object);
+        }
+    }
+
+    @Override
+    public void adjustContextAfterMessageSent(SshContext context, T object) {
+        if (Converter.byteToBoolean(object.getWantReply().getValue())) {
+            context.getChannelManager().addSentRequestThatWantsReply(object);
+        }
+    }
+}
